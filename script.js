7f8a8f9b9d67ece2ae56facef2c062068ba230fd
piximr <piximrstudio@gmail.com>
2026-07-18 22:23:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+window.addEventListener("load", () => {
+
+    const hero = document.querySelector(".hero-content");
+
+    hero.style.opacity = "0";
+    hero.style.transform = "translateY(30px)";
+
+    setTimeout(() => {
+
+        hero.style.transition = "1.2s ease";
+        hero.style.opacity = "1";
+        hero.style.transform = "translateY(0)";
+
+    }, 300);
+
+});
