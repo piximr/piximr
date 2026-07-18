7cee1f29ae212a4e9cad55f7113e9d578477003d
piximr <piximrstudio@gmail.com>
2026-07-19 04:34:17 +0600

    Create piximr-ai-data.js

---
+++ piximr-ai-data.js
@@ -0,0 +1,32 @@
+const piximrAI = {
+
+name: "Piximr AI",
+
+intro:
+"Hello Sir, I'm Piximr AI. It's a pleasure to meet you. I’d love to know what brings you to Piximr today.",
+
+
+about:
+"Piximr is a creative visualization studio focused on transforming ideas into powerful visual experiences.",
+
+
+services:[
+"Architectural Visualization",
+"3D Rendering",
+"3D Animation",
+"Product Visualization",
+"Creative Design"
+],
+
+
+strength:
+"Piximr combines artistic vision with modern 3D technology to create visuals with emotion, storytelling and detail.",
+
+
+price:
+"Every project has unique creative requirements. I would love to connect you with our artist so we can understand your vision better.",
+
+
+contact:
+"I would be happy to connect you with our creative team. Please share your project details and contact information."
+};
